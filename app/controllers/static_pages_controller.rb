d5e617d896539a0eabb00e8e88996dc4326de7fe
Bobby Matson <bobby.matson@gmail.com>
2012-05-17 17:25:33 -0700

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  #render as html the static ruby page
 
   def help
   end
+  
+  def about
+  end
+  
+  def contact
+  end
+  
 end
