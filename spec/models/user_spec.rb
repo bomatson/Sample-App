b02c8362f0e7f285daa183d99646b63373b813ac
Bobby Matson <bobby.matson@gmail.com>
2012-05-22 18:05:19 -0700

    Adjusted some regex

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,8 @@ describe User do
   
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                    user@foo_bar.com foo@bar+biz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
