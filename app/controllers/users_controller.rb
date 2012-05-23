8e81a1642c555faa0c08fbc026b8b8fa704691d4
Bobby Matson <bobby.matson@gmail.com>
2012-05-23 11:52:24 -0700

    Finish the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save 
+      flash[:success] = "Welcome to this Sample App, yo!"
+      redirect_to @user #same as user_path(@user)
+    else
+      render 'new'
+    end
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+ factory :user do
+  name "Bobby Matson"
+  email "bobby.matson@hotmail.com"
+  password "foobar"
+  password_confirmation "foobar"
+ end
+end
