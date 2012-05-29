9e6785362e20cbdbe033ba923e0b813c433539f3
Bobby Matson <bobby.matson@gmail.com>
2012-05-29 16:48:41 -0700

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,4 +43,11 @@ module SessionsHelper
     session.delete(:return_to)
   end
   
+  def signed_in_user
+    unless signed_in?
+      store_location
+    redirect_to signin_path, notice: "Please sign in" unless signed_in?
+    end
+  end
+  
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
  factory :user do
   sequence(:name) {|n| "Person #{n}"}
-  sequence(:email) {|n| "person_#{n+1}@hotmail.com"}
+  sequence(:email) {|n| "person_#{n}@hotmail.com"}
   password "foobar"
   password_confirmation "foobar"
   
@@ -9,4 +9,9 @@ FactoryGirl.define do
     admin true
   end
  end
+  
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
