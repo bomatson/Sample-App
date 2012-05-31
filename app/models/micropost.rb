78b79641f6b1b5885eb3120eaebd7db9b5db06ae
Bobby Matson <bobby.matson@gmail.com>
2012-05-30 18:07:03 -0700

    Add User following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,15 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
 
   default_scope order: 'microposts.created_at DESC'
+  
+  def self.from_users_followed_by(user)
+    # following_ids = user.followed_users.map(&:id).join(', ')
+    followed_user_ids = "SELECT followed_id FROM relationships
+                        WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
 end
+
+
+#[1, 2, 3, 4].map {|i| i.to_s}
